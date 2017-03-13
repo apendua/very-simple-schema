90f6bb8b99a0a7888c03169d10733ca05efa7afe
Tomasz Lenarcik <apendua@gmail.com>
2017-03-13 14:36:59 +0100

    Add preset basic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import createSchema from './createSchema.js';
+import presetBasic from './plugins/presetBasic.js';
 import presetDefault from './plugins/presetDefault.js';
 import Schema from './Schema';
 
 export {
   Schema,
   createSchema,
+  presetBasic,
   presetDefault,
 };
---
+++ src/plugins/presetBasic.js
@@ -0,0 +1,13 @@
+import pluginAllowedValues from './allowedValues.js';
+import pluginAtomic from './atomic.js';
+import pluginArray from './array.js';
+import pluginObject from './object.js';
+import pluginRegExp from './regExp.js';
+
+export default [
+  pluginObject,
+  pluginArray,
+  pluginAtomic,
+  pluginAllowedValues,
+  pluginRegExp,
+];
