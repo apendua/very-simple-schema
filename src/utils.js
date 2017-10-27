4c5af4d97f1cf5861358b857b2394af799de8f0d
Tomasz Lenarcik <apendua@gmail.com>
2017-10-27 14:00:05 +0200

    Cleanup utils exports

--- src/utils.js
+++ src/utils.js
@@ -4,8 +4,8 @@ import isPlainObject from 'is-plain-object';
 const hasOwnProperty = Object.prototype.hasOwnProperty;
 const toString = Object.prototype.toString;
 
-export const has = (object, property) => hasOwnProperty.call(object, property);
-export const each = (object, action) => {
+const has = (object, property) => hasOwnProperty.call(object, property);
+const each = (object, action) => {
   if (object && typeof object === 'object') {
     // eslint-disable-next-line no-restricted-syntax
     for (const key in object) {
@@ -16,8 +16,8 @@ export const each = (object, action) => {
   }
 };
 
-export const isArray = actual => Array.isArray(actual);
-export const isDate = actual => toString.call(actual) === '[object Date]';
+const isArray = actual => Array.isArray(actual);
+const isDate = actual => toString.call(actual) === '[object Date]';
 
 const noop = () => {};
 export const combine = validators => validators.reduce(
@@ -28,6 +28,10 @@ export const combine = validators => validators.reduce(
 );
 
 export {
+  isDate,
+  each,
+  has,
+  isArray,
   isObject,
   isPlainObject,
 };
